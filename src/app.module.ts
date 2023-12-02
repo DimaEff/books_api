12e88a280d45768545089871cc2aef8a5421e160
Daniil Morozov <dan.project1101@gmail.com>
2023-12-03 01:01:06 +0300

    connected to the db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ConfigModule } from '@nestjs/config';
-import { appConfig, databaseConfig } from '@/config';
+import { appConfig } from '@/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeConfigModule } from './sequelize-config/sequelize-config.module';
+import { SequelizeConfigService } from '@/sequelize-config/sequelize-config.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [appConfig, databaseConfig] })],
+  imports: [
+    ConfigModule.forRoot({ load: [appConfig] }),
+    SequelizeConfigModule,
+    // SequelizeModule.forRoot({
+    //   dialect: 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    //   models: [],
+    // }),
+    SequelizeModule.forRootAsync({
+      imports: [SequelizeConfigModule],
+      useExisting: SequelizeConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
