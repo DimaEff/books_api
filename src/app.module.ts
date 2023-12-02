4d89444c0b8a264049242933d60fd3fab3a029fe
Daniil Morozov <dan.project1101@gmail.com>
2023-12-02 16:58:45 +0300

    refactored app .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ConfigModule } from '@nestjs/config';
 import databaseConfig from '@/config/database.config';
+import appConfig from '@/config/app.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [databaseConfig] })],
+  imports: [ConfigModule.forRoot({ load: [appConfig, databaseConfig] })],
   controllers: [AppController],
   providers: [AppService],
 })
