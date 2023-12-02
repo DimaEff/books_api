eb25639ab2055412f95f246464da2746228794ee
Daniil Morozov <dan.project1101@gmail.com>
2023-12-02 16:41:35 +0300

    added a .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from '@/config/database.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ load: [databaseConfig] })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import databaseConfig from '@/config/database.config';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject(databaseConfig.KEY)
+    private dbConfig: ConfigType<typeof databaseConfig>,
+  ) {}
+
+  getHello() {
+    const dbPort = this.dbConfig.DB_PORT;
+    const dbHost = this.dbConfig.DB_HOST;
+    return { dbPort, dbHost };
   }
 }
